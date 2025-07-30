013c6795dbb2a60d77c5a9f94ea2cdf82ca7b283
Stasik <goneinterlude@gmail.com>
2025-07-30 08:44:55 +0300

    new update 30.07

---
+++ node-express/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const path = require("path");
+const { title } = require("process");
+const homeRoutes = require("./routes/home");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
