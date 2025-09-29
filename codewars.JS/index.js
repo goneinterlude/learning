61b7350a2da759e4a5923fdaeac9d213b806aac1
Stasik <goneinterlude@gmail.com>
2025-09-29 13:19:11 +0300

    commit 29.09 part 2

--- codewars.JS/index.js
+++ codewars.JS/index.js
@@ -8,3 +8,17 @@ The input string will only consist of lower case letters and/or spaces.
 const stringToNumber = function(str){
   return Number(str);
 }
+
+
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+
+function countBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i++) {
+  z.push(x * i)}
+  return z;
+}
