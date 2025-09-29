c1762385ddb59289d40ba2efcd6496ff55631bcf
Stasik <goneinterlude@gmail.com>
2025-09-29 14:00:00 +0300

    commit 29.09 part 3

--- codewars.JS/index.js
+++ codewars.JS/index.js
@@ -22,3 +22,11 @@ function countBy(x, n) {
   z.push(x * i)}
   return z;
 }
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+    if (number % 2 === 0) {
+  return number * 8} else {
+  return number * 9}
+}
