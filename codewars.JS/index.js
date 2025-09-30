0dde9d3c92e980c975616ec88083c57344a47563
Stasik <goneinterlude@gmail.com>
2025-09-30 12:19:40 +0300

    commit 30.09

--- codewars.JS/index.js
+++ codewars.JS/index.js
@@ -41,3 +41,12 @@ function findUniq(arr) {
     }
   }
 }
+
+
+function findNeedle(haystack) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === 'needle') {
+      return `found the needle at position ${i}`;
+    }
+  }
+}
